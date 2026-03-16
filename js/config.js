46a4d468e2465efd7820fd1a0d4b4ba68a996ba0
heartfulnessgujarat <heartfulness.gujarat@gmail.com>
2026-03-16 20:55:45 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
-const CONFIG = {
+console.log("Config loaded");
 
-API_URL:
-https://script.google.com/macros/s/AKfycbzjB4tS9H87lBZeYXEWbotv1R3tgFdp5X2iekYEGQ8JjLSiJvd18Q1SrZ3p8dN7mtrC/exec
-  
+var CONFIG = {
+
+API_URL:"https://script.google.com/macros/s/AKfycbzjB4tS9H87lBZeYXEWbotv1R3tgFdp5X2iekYEGQ8JjLSiJvd18Q1SrZ3p8dN7mtrC/exec"
 
 };
