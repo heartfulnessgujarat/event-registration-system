471f170ed33cb72b5a5f96514bb57099a3467d60
heartfulnessgujarat <heartfulness.gujarat@gmail.com>
2026-03-16 20:34:51 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
 
 API_URL:
-"https://script.google.com/macros/s/AKfycbxxxxxxxxxxxx/exec"
+https://script.google.com/macros/s/AKfycbzjB4tS9H87lBZeYXEWbotv1R3tgFdp5X2iekYEGQ8JjLSiJvd18Q1SrZ3p8dN7mtrC/exec
+  
 
 };
