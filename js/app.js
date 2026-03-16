4de56ed60d39453bdb8566cf4194a95d6abaa9dd
heartfulnessgujarat <heartfulness.gujarat@gmail.com>
2026-03-16 23:43:01 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,48 +8,74 @@ async function(){
 
 let query=this.value;
 
-if(query.length<3)
+if(query.length<3){
+
+document
+.getElementById("suggestions")
+.innerHTML="";
+
 return;
+
+}
+
+try{
 
 let response =
 await fetch(
-CONFIG.API_URL,
-{
-method:"POST",
 
-headers:{
-"Content-Type":
-"application/x-www-form-urlencoded"
-},
+CONFIG.API_URL +
+"?query="+
+encodeURIComponent(query)
 
-body:
-"action=search&query="+query
-
-}
 );
 
-let text =
-await response.text();
-
 let data =
-JSON.parse(text);
+await response.json();
+
+if(data.status=="success"){
 
 showSuggestions(data.data);
 
 }
+
+}
+catch(error){
+
+console.log("Error:",error);
+
+}
+
+}
 );
+
+
 
 function showSuggestions(list){
 
 let html="";
 
+if(list.length==0){
+
+html=
+"<div class='suggestionItem'>No participant found</div>";
+
+}
+
+else{
+
 for(let i=0;i<list.length;i++){
 
 html+=`
+
 <div class="suggestionItem">
+
 ${list[i]}
+
 </div>
+
 `;
+
+}
 
 }
 
