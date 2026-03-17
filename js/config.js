944c8c38774bc1cd41dcc58ecbe770ceaed3d0ed
heartfulnessgujarat <heartfulness.gujarat@gmail.com>
2026-03-17 19:04:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,11 @@
 console.log("CONFIG FILE LOADED");
 
-
 window.CONFIG = {
 
 API_URL:
-"https://floral-cherry-cef9.heartfulness-gujarat.workers.dev/"
+"https://floral-cherry-cef9.heartfulness-gujarat.workers.dev/",
+
+THANKYOU_URL:
+"https://docs.google.com/document/d/e/2PACX-XXXXXXXX/pub?embedded=true"
 
 };
